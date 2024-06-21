50c80eff055e316c090c785f24932850dc4a336f
Malvirino Hardian <hardianmalvirino@gmail.com>
2024-06-22 00:30:32 +0700

    modal1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}", 
+  "./node_modules/flowbite/**/*.js"],
   theme: {
     colors: {
       navy: "#16215B",
