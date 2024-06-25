5704c360c25259bd8679445bb238dacff2b703cc
Malvirino Hardian <hardianmalvirino@gmail.com>
2024-06-25 17:49:55 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", 
-  "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    
     colors: {
       navy: "#16215B",
       orange1: "#F25929",
@@ -16,6 +19,7 @@ module.exports = {
       "b-red": "#EF4444",
       "b-blue": "#3B82F6",
       "b-gr": "#3B82F6",
+      "m-gr": "#C5C5C5",
     },
     extend: {},
     daisyui: {
